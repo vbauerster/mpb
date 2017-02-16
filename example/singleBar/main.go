3832d219e758a8ca278bba1eee696c41c5ec4c12
Vladimir Bauer <vbauerster@gmail.com>
2017-02-16 21:33:21 +0500

    use custom bar format

--- example/singleBar/main.go
+++ example/singleBar/main.go
@@ -15,6 +15,9 @@ func main() {
 	// If you don't plan to cancel, feed with nil
 	// otherwise provide context.Context, see cancel example
 	p := mpb.New(nil)
+	// Set custom format, the default one is "[=>-]"
+	p.Format("╢▌▌░╟")
+
 	bar := p.AddBar(100).PrependName(name, 0).AppendPercentage()
 
 	for i := 0; i < 100; i++ {
