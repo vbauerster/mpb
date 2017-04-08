74db5f71beb1b757f7989c265a6e9615b5d14b95
Vladimir Bauer <vbauerster@gmail.com>
2017-04-08 17:01:27 +0500

    Copyright note

--- doc.go
+++ doc.go
@@ -1,5 +1,6 @@
-// Package mpb is a library for rendering progress bars in terminal applications.
 // Copyright (C) 2016-2017 Vladimir Bauer
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+// Package mpb is a library for rendering progress bars in terminal applications.
 package mpb
