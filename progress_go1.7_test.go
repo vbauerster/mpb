d4b95140ce4af101b8c640c1527884e0c37cc2a3
Vladimir Bauer <vbauerster@gmail.com>
2017-05-20 23:30:00 +0300

    TestWithContext with +build go1.7 flag

---
+++ progress_go1.7_test.go
@@ -0,0 +1,39 @@
+//+build go1.7
+package mpb_test
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/vbauerster/mpb"
+)
+
+func TestWithContext(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	shutdown := make(chan struct{})
+	p := mpb.New().WithContext(ctx).ShutdownNotify(shutdown)
+	numBars := 3
+
+	for i := 0; i < numBars; i++ {
+		name := fmt.Sprintf("Bar#%d:", i)
+		bar := p.AddBar(100).PrependName(name, len(name), 0)
+
+		go func() {
+			for i := 0; i < 10000; i++ {
+				bar.Incr(1)
+				time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+			}
+		}()
+	}
+
+	cancel()
+
+	select {
+	case <-shutdown:
+	case <-time.After(500 * time.Millisecond):
+		t.Error("ProgressBar didn't stop")
+	}
+}
--- progress_test.go
+++ progress_test.go
@@ -2,7 +2,6 @@ package mpb_test
 
 import (
 	"bytes"
-	"context"
 	"fmt"
 	"math/rand"
 	"strings"
@@ -97,33 +96,6 @@ func TestRemoveBar(t *testing.T) {
 		t.Errorf("BarCount want: %q, got: %q\n", 0, count)
 	}
 	p.Stop()
-}
-
-func TestWithContext(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	shutdown := make(chan struct{})
-	p := mpb.New().WithContext(ctx).ShutdownNotify(shutdown)
-	numBars := 3
-
-	for i := 0; i < numBars; i++ {
-		name := fmt.Sprintf("Bar#%d:", i)
-		bar := p.AddBar(100).PrependName(name, len(name), 0)
-
-		go func() {
-			for i := 0; i < 10000; i++ {
-				bar.Incr(1)
-				time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-			}
-		}()
-	}
-
-	cancel()
-
-	select {
-	case <-shutdown:
-	case <-time.After(500 * time.Millisecond):
-		t.Error("ProgressBar didn't stop")
-	}
 }
 
 func TestWithCancel(t *testing.T) {
