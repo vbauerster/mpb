387ee314aebf16ba5641ef0dcf9cc224db046a2d
Vladimir Bauer <vbauerster@gmail.com>
2017-05-23 11:55:53 +0300

    TestPrependCountersDindentRight

--- decorators_test.go
+++ decorators_test.go
@@ -68,11 +68,35 @@ func TestPrependCounters(t *testing.T) {
 
 	p.Stop()
 
-	out := buf.String()
-	out = out[:strings.IndexRune(out, '[')]
-	want := fmt.Sprintf("%[1]db / %[1]db", total)
-	if out != want {
-		t.Errorf("Expected: %s, got %s\n", want, out)
+	barOut := buf.String()
+	want := fmt.Sprintf("%[1]db / %[1]db[", total)
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
+	}
+}
+
+func TestPrependCountersDindentRight(t *testing.T) {
+	var buf bytes.Buffer
+	p := mpb.New().SetOut(&buf)
+
+	reader := strings.NewReader(content)
+
+	total := int64(len(content))
+	bar := p.AddBar(total).TrimLeftSpace().TrimRightSpace().
+		PrependCounters("%3s / %3s", mpb.UnitBytes, 12, mpb.DidentRight)
+	preader := bar.ProxyReader(reader)
+
+	_, err := io.Copy(ioutil.Discard, preader)
+	if err != nil {
+		t.Errorf("Error copying from reader: %+v\n", err)
+	}
+
+	p.Stop()
+
+	barOut := buf.String()
+	want := fmt.Sprintf("%[1]db / %[1]db [", total)
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
 	}
 }
 
