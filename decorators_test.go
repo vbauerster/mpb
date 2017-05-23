467cf45ad031ef03ed878d6c8005fca8e9b7455a
Vladimir Bauer <vbauerster@gmail.com>
2017-05-23 10:55:31 +0300

    DindentRight

--- decorators_test.go
+++ decorators_test.go
@@ -15,14 +15,38 @@ import (
 func TestPrependName(t *testing.T) {
 	var buf bytes.Buffer
 	p := mpb.New().SetOut(&buf)
-	wantName := "TestBar"
-	bar := p.AddBar(100).PrependName(wantName, 0, 0)
+	name := "TestBar"
+	bar := p.AddBar(100).TrimLeftSpace().TrimRightSpace().
+		PrependName(name, 0, 0)
 	for i := 0; i < 100; i++ {
 		bar.Incr(1)
 	}
+
 	p.Stop()
-	if !strings.Contains(buf.String(), wantName) {
-		t.Errorf("%q not found in bar\n", wantName)
+
+	want := name + "["
+	barOut := buf.String()
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
+	}
+}
+
+func TestPrependNameDindentRight(t *testing.T) {
+	var buf bytes.Buffer
+	p := mpb.New().SetOut(&buf)
+	name := "TestBar"
+	bar := p.AddBar(100).TrimLeftSpace().TrimRightSpace().
+		PrependName(name, len(name)+1, mpb.DidentRight)
+	for i := 0; i < 100; i++ {
+		bar.Incr(1)
+	}
+
+	p.Stop()
+
+	want := name + " ["
+	barOut := buf.String()
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
 	}
 }
 
@@ -57,7 +81,7 @@ func TestAppendPercentage(t *testing.T) {
 	p := mpb.New().SetOut(&buf)
 
 	bar := p.AddBar(100).TrimLeftSpace().TrimRightSpace().
-		AppendPercentage(0, 0)
+		AppendPercentage(6, 0)
 
 	for i := 0; i < 100; i++ {
 		time.Sleep(10 * time.Millisecond)
@@ -66,9 +90,31 @@ func TestAppendPercentage(t *testing.T) {
 
 	p.Stop()
 
-	want := "]100 %"
-	if !strings.Contains(buf.String(), want) {
-		t.Errorf("%q not found in bar\n", want)
+	want := "] 100 %"
+	barOut := buf.String()
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
+	}
+}
+
+func TestAppendPercentageDindentRight(t *testing.T) {
+	var buf bytes.Buffer
+	p := mpb.New().SetOut(&buf)
+
+	bar := p.AddBar(100).TrimLeftSpace().TrimRightSpace().
+		AppendPercentage(6, mpb.DidentRight)
+
+	for i := 0; i < 100; i++ {
+		time.Sleep(10 * time.Millisecond)
+		bar.Incr(1)
+	}
+
+	p.Stop()
+
+	want := "]100 % "
+	barOut := buf.String()
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
 	}
 }
 
@@ -87,8 +133,9 @@ func TestPrependPercentage(t *testing.T) {
 	p.Stop()
 
 	want := " 100 %["
-	if !strings.Contains(buf.String(), want) {
-		t.Errorf("%q not found in bar\n", want)
+	barOut := buf.String()
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
 	}
 }
 
@@ -107,7 +154,8 @@ func TestPrependPercentageDindentRight(t *testing.T) {
 	p.Stop()
 
 	want := "100 % ["
-	if !strings.Contains(buf.String(), want) {
-		t.Errorf("%q not found in bar\n", want)
+	barOut := buf.String()
+	if !strings.Contains(barOut, want) {
+		t.Errorf("%q not found in bar: %s\n", want, barOut)
 	}
 }
