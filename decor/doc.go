3cd56974cc21d5a455ad6457de6bb0dba09a48b4
Vladimir Bauer <vbauerster@gmail.com>
2018-04-28 23:25:19 +0300

    decor doc upd

--- decor/doc.go
+++ decor/doc.go
@@ -3,23 +3,24 @@
 // license that can be found in the LICENSE file.
 
 /*
-	Package decor contains common decorators for "github.com/vbauerster/mpb" package.
+ Package decor contains common decorators for "github.com/vbauerster/mpb" package.
 
-    All decorators returned by this package have some closure state. It is ok to use decorators concurrently,
-    unless you share the same decorator among multiple *mpb.Bar instances. To avoid data races, create new
-    decorator per *mpb.Bar instance.
+ All decorators returned by this package have some closure state. It is ok to use
+ decorators concurrently, unless you share the same decorator among multiple
+ *mpb.Bar instances. To avoid data races, create new decorator per *mpb.Bar
+ instance.
 
-	Don't:
+ Don't:
 
-          p := mpb.New()
-          eta := decor.ETA(0, 0)
-          p.AddBar(100, mpb.AppendDecorators(eta))
-          p.AddBar(100, mpb.AppendDecorators(eta))
+	  p := mpb.New()
+	  eta := decor.ETA(0, 0)
+	  p.AddBar(100, mpb.AppendDecorators(eta))
+	  p.AddBar(100, mpb.AppendDecorators(eta))
 
-	Do:
+ Do:
 
-          p := mpb.New()
-          p.AddBar(100, mpb.AppendDecorators(decor.ETA(0, 0)))
-          p.AddBar(100, mpb.AppendDecorators(decor.ETA(0, 0)))
+	p := mpb.New()
+	p.AddBar(100, mpb.AppendDecorators(decor.ETA(0, 0)))
+	p.AddBar(100, mpb.AppendDecorators(decor.ETA(0, 0)))
 */
 package decor
