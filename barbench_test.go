c53a12830ed54b442064831a5ba966935a27956e
Vladimir Bauer <vbauerster@gmail.com>
2018-06-03 02:26:22 +0300

    refactor benchmark

--- barbench_test.go
+++ barbench_test.go
@@ -5,38 +5,27 @@ import (
 	"testing"
 )
 
-func benchmarkSingleBar(total int) {
-	p := New(WithOutput(ioutil.Discard))
-	bar := p.AddBar(int64(total))
-	for i := 0; i < total; i++ {
-		bar.Increment()
-	}
-	p.Wait()
-}
-
-func BenchmarkSingleBar100(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchmarkSingleBar(100)
-	}
-}
-
-func BenchmarkSingleBar1000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchmarkSingleBar(1000)
-	}
-}
-
-func BenchmarkSingleBar10000(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		benchmarkSingleBar(10000)
-	}
-}
-
 func BenchmarkIncrSingleBar(b *testing.B) {
 	p := New(WithOutput(ioutil.Discard))
 	bar := p.AddBar(int64(b.N))
 	for i := 0; i < b.N; i++ {
 		bar.Increment()
 	}
-	p.Wait()
+}
+
+func BenchmarkIncrSingleBarStartBlock(b *testing.B) {
+	p := New(WithOutput(ioutil.Discard))
+	bar := p.AddBar(int64(b.N))
+	for i := 0; i < b.N; i++ {
+		bar.StartBlock()
+		bar.Increment()
+	}
+}
+
+func BenchmarkIncrSingleBarWhileIsNotCompleted(b *testing.B) {
+	p := New(WithOutput(ioutil.Discard))
+	bar := p.AddBar(int64(b.N))
+	for !bar.Completed() {
+		bar.Increment()
+	}
 }
