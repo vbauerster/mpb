144df0819a7793854477f073a2d5fad6cab0312b
Vladimir Bauer <vbauerster@gmail.com>
2018-06-12 00:10:04 +0300

    decor package top godoc update

--- decor/doc.go
+++ decor/doc.go
@@ -5,22 +5,21 @@
 /*
  Package decor contains common decorators for "github.com/vbauerster/mpb" package.
 
- All decorators returned by this package have some closure state. It is ok to use
+ Some decorators returned by this package might have a closure state. It is ok to use
  decorators concurrently, unless you share the same decorator among multiple
- *mpb.Bar instances. To avoid data races, create new decorator per *mpb.Bar
- instance.
+ *mpb.Bar instances. To avoid data races, create new decorator per *mpb.Bar instance.
 
  Don't:
 
 	 p := mpb.New()
-	 eta := decor.ETA(0, 0)
+	 eta := decor.ETA(decor.ET_STYLE_GO, 0, startBlock)
 	 p.AddBar(100, mpb.AppendDecorators(eta))
 	 p.AddBar(100, mpb.AppendDecorators(eta))
 
  Do:
 
 	p := mpb.New()
-	p.AddBar(100, mpb.AppendDecorators(decor.ETA(0, 0)))
-	p.AddBar(100, mpb.AppendDecorators(decor.ETA(0, 0)))
+	p.AddBar(100, mpb.AppendDecorators(decor.ETA(decor.ET_STYLE_GO, 0, startBlock)))
+	p.AddBar(100, mpb.AppendDecorators(decor.ETA(decor.ET_STYLE_GO, 0, startBlock)))
 */
 package decor
