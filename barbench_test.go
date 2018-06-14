026348035cb2a9cc885794f759f962d828c79c0b
Vladimir Bauer <vbauerster@gmail.com>
2018-06-14 19:53:10 +0300

    Add bench with name decorator

--- barbench_test.go
+++ barbench_test.go
@@ -3,6 +3,8 @@ package mpb
 import (
 	"io/ioutil"
 	"testing"
+
+	"github.com/vbauerster/mpb/decor"
 )
 
 func BenchmarkIncrSingleBar(b *testing.B) {
@@ -17,6 +19,14 @@ func BenchmarkIncrSingleBarWhileIsNotCompleted(b *testing.B) {
 	p := New(WithOutput(ioutil.Discard))
 	bar := p.AddBar(int64(b.N))
 	for !bar.Completed() {
+		bar.Increment()
+	}
+}
+
+func BenchmarkIncrSingleBarWithNameDecorator(b *testing.B) {
+	p := New(WithOutput(ioutil.Discard))
+	bar := p.AddBar(int64(b.N), PrependDecorators(decor.Name("test")))
+	for i := 0; i < b.N; i++ {
 		bar.Increment()
 	}
 }
