63e67c58a64d16ad68ed784b1a0e6a39230a3579
Vladimir Bauer <vbauerster@gmail.com>
2018-06-15 11:58:23 +0300

    ctx must not be nil

--- options_go1.7.go
+++ options_go1.7.go
@@ -7,8 +7,9 @@ import "context"
 // WithContext provided context will be used for cancellation purposes
 func WithContext(ctx context.Context) ProgressOption {
 	return func(s *pState) {
-		if ctx != nil {
-			s.cancel = ctx.Done()
+		if ctx == nil {
+			panic("ctx must not be nil")
 		}
+		s.cancel = ctx.Done()
 	}
 }
