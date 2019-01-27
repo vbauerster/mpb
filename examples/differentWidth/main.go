0a29900ab538911b435b18e9c334953433431807
Vladimir Bauer <vbauerster@gmail.com>
2019-01-28 01:45:43 +0300

    different width example

---
+++ examples/differentWidth/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/vbauerster/mpb"
+	"github.com/vbauerster/mpb/decor"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	var wg sync.WaitGroup
+	p := mpb.New(
+		mpb.WithWaitGroup(&wg),
+		// container's width.
+		mpb.WithWidth(60),
+	)
+	total, numBars := 100, 3
+	wg.Add(numBars)
+
+	for i := 0; i < numBars; i++ {
+		name := fmt.Sprintf("Bar#%d:", i)
+		bar := p.AddBar(int64(total),
+			// set BarWidth 40 for bar 1 and 2
+			mpb.OptionOnCondition(mpb.BarWidth(40), func() bool { return i > 0 }),
+			mpb.PrependDecorators(
+				// simple name decorator
+				decor.Name(name),
+				// decor.DSyncWidth bit enables column width synchronization
+				decor.Percentage(decor.WCSyncSpace),
+			),
+			mpb.AppendDecorators(
+				// replace ETA decorator with "done" message, OnComplete event
+				decor.OnComplete(
+					// ETA decorator with ewma age of 60
+					decor.EwmaETA(decor.ET_STYLE_GO, 60), "done",
+				),
+			),
+		)
+		// simulating some work
+		go func() {
+			defer wg.Done()
+			max := 100 * time.Millisecond
+			for i := 0; i < total; i++ {
+				start := time.Now()
+				time.Sleep(time.Duration(rand.Intn(10)+1) * max / 10)
+				// ewma based decorators require work duration measurement
+				bar.IncrBy(1, time.Since(start))
+			}
+		}()
+	}
+	// wait for all bars to complete and flush
+	p.Wait()
+}
