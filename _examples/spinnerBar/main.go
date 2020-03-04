010bd5c8c51531a9ee99b8291e524adcf8b7d449
Vladimir Bauer <vbauerster@gmail.com>
2020-03-04 20:38:21 +0500

    example update

--- _examples/spinnerBar/main.go
+++ _examples/spinnerBar/main.go
@@ -62,10 +62,13 @@ func main() {
 			rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 			max := 100 * time.Millisecond
 			for i := 0; i < total; i++ {
+				// start variable is solely for EWMA calculation
+				// EWMA's unit of measure is an iteration's taken time
 				start := time.Now()
 				time.Sleep(time.Duration(rng.Intn(10)+1) * max / 10)
-				// since ewma decorator is used, we need to pass time.Since(start)
-				bar.Increment(time.Since(start))
+				bar.Increment()
+				// we need to call DecoratorEwmaUpdate to fulfill ewma decorator's contract
+				bar.DecoratorEwmaUpdate(time.Since(start))
 			}
 		}()
 	}
