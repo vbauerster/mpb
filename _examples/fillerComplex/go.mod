2f1919557fbd76982cf11bad4be53a001b2b3cc4
Mexican-Man <colecrouter@hotmail.com>
2021-03-26 18:09:02 -0600

    Add files via upload

---
+++ _examples/fillerComplex/go.mod
@@ -0,0 +1,5 @@
+module github.com/vbauerster/mpb/_examples/fillerComplex
+
+go 1.14
+
+require github.com/vbauerster/mpb/v6 v6.0.2
---
+++ _examples/fillerComplex/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/vbauerster/mpb/v6"
+	"github.com/vbauerster/mpb/v6/decor"
+)
+
+func main() {
+	// initialize progress container, with custom width
+	p := mpb.New(mpb.WithWidth(64))
+
+	total := 100
+	name := "Complex Filler:"
+	bar := p.Add(int64(total),
+		// NewBarComplexFiller allows for multi-rune strings
+		mpb.NewBarComplexFiller("[\u001b[36;1m", "_", "\u001b[0m⛵\u001b[36;1m", "_", "\u001b[0m]"),
+		mpb.PrependDecorators(
+			// simple name decorator
+			decor.Name(name),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
+	// simulating some work
+	max := 100 * time.Millisecond
+	for i := 0; i < total; i++ {
+		time.Sleep(time.Duration(rand.Intn(10)+1) * max / 10)
+		bar.Increment()
+	}
+	// wait for our bar to complete
+	p.Wait()
+}
