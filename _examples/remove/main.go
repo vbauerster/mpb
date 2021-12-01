086ddfabd20d0ed7b1429e40382c1cab4d4cf008
Vladimir Bauer <vbauerster@gmail.com>
2021-12-01 15:48:10 +0500

    introduce OnAbort decorator wrapper
    address issue #103

--- _examples/remove/main.go
+++ _examples/remove/main.go
@@ -24,7 +24,6 @@ func main() {
 			mpb.BarOptional(mpb.BarRemoveOnComplete(), i == 0),
 			mpb.PrependDecorators(
 				decor.Name(name),
-				decor.EwmaETA(decor.ET_STYLE_GO, 60, decor.WCSyncSpace),
 			),
 			mpb.AppendDecorators(
 				decor.Any(func(s decor.Statistics) string {
@@ -33,7 +32,8 @@ func main() {
 				decor.Any(func(s decor.Statistics) string {
 					return fmt.Sprintf("aborted: %v", s.Aborted)
 				}, decor.WCSyncSpaceR),
-				decor.OnComplete(decor.Percentage(decor.WCSyncSpace), "done"),
+				decor.OnComplete(decor.NewPercentage("%d", decor.WCSyncSpace), "done"),
+				decor.OnAbort(decor.NewPercentage("%d", decor.WCSyncSpace), "ohno"),
 			),
 		)
 		go func() {
@@ -41,16 +41,11 @@ func main() {
 			rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 			max := 100 * time.Millisecond
 			for i := 0; !bar.Completed(); i++ {
-				// start variable is solely for EWMA calculation
-				// EWMA's unit of measure is an iteration's duration
-				start := time.Now()
 				if bar.ID() == 2 && i >= 42 {
 					bar.Abort(false)
 				}
 				time.Sleep(time.Duration(rng.Intn(10)+1) * max / 10)
 				bar.Increment()
-				// we need to call DecoratorEwmaUpdate to fulfill ewma decorator's contract
-				bar.DecoratorEwmaUpdate(time.Since(start))
 			}
 		}()
 	}
