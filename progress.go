342d4f2c68affe1a0afd2acb9a349da76b73a553
Vladimir Bauer <vbauerster@gmail.com>
2022-09-19 12:57:00 +0500

    rename method Add to AddFiller

--- progress.go
+++ progress.go
@@ -114,13 +114,13 @@ func (p *Progress) AddSpinner(total int64, options ...BarOption) *Bar {
 
 // New creates a bar with provided BarFillerBuilder.
 func (p *Progress) New(total int64, builder BarFillerBuilder, options ...BarOption) *Bar {
-	return p.Add(total, builder.Build(), options...)
+	return p.AddFiller(total, builder.Build(), options...)
 }
 
-// Add creates a bar which renders itself by provided filler.
+// AddFiller creates a bar which renders itself by provided filler.
 // If `total <= 0` triggering complete event by increment methods is disabled.
 // Panics if *Progress instance is done, i.e. called after (*Progress).Wait().
-func (p *Progress) Add(total int64, filler BarFiller, options ...BarOption) *Bar {
+func (p *Progress) AddFiller(total int64, filler BarFiller, options ...BarOption) *Bar {
 	if filler == nil {
 		filler = NopStyle().Build()
 	}
