69d71ef6c89674e919f6733ee14f59e76a536ad2
Vladimir Bauer <vbauerster@gmail.com>
2023-04-07 02:48:22 +0500

    refactoring percentage
    accept any verb which is supported by AppendFloat

--- decor/percentage.go
+++ decor/percentage.go
@@ -12,20 +12,20 @@ var _ fmt.Formatter = percentageType(0)
 type percentageType float64
 
 func (s percentageType) Format(st fmt.State, verb rune) {
-	var prec int
+	prec := -1
 	switch verb {
-	case 'd':
-	case 's':
-		prec = -1
-	default:
+	case 'f', 'e', 'E':
+		prec = 6 // default prec of fmt.Printf("%f|%e|%E")
+		fallthrough
+	case 'b', 'g', 'G', 'x', 'X':
 		if p, ok := st.Precision(); ok {
 			prec = p
-		} else {
-			prec = 6
 		}
+	default:
+		verb, prec = 'f', 0
 	}
 
-	b := strconv.AppendFloat(make([]byte, 0, 32), float64(s), 'f', prec, 64)
+	b := strconv.AppendFloat(make([]byte, 0, 16), float64(s), byte(verb), prec, 64)
 	if st.Flag(' ') {
 		b = append(b, ' ', '%')
 	} else {
@@ -44,12 +44,18 @@ func Percentage(wcc ...WC) Decorator {
 
 // NewPercentage percentage decorator with custom format string.
 //
+//	`format` printf compatible verb
+//
+//	`wcc` optional WC config
+//
 // format examples:
 //
-//	format="%.1f"  output: "1.0%"
-//	format="% .1f" output: "1.0 %"
 //	format="%d"    output: "1%"
 //	format="% d"   output: "1 %"
+//	format="%.1f"  output: "1.0%"
+//	format="% .1f" output: "1.0 %"
+//	format="%f"    output: "1.000000%"
+//	format="% f"   output: "1.000000 %"
 func NewPercentage(format string, wcc ...WC) Decorator {
 	if format == "" {
 		format = "% d"
