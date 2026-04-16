7db72291120fa8ad605d882f7a92e4e5a923c117
Vladimir Bauer <vbauerster@gmail.com>
2026-04-16 16:28:59 +0500

    refactor: proxyreader_test

--- proxyreader_test.go
+++ proxyreader_test.go
@@ -2,6 +2,7 @@ package mpb_test
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"strings"
 	"testing"
@@ -18,29 +19,25 @@ const content = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed d
 		est laborum.`
 
 type testReader struct {
-	io.Reader
+	r      io.Reader
 	called bool
 }
 
 func (r *testReader) Read(p []byte) (n int, err error) {
 	r.called = true
-	return r.Reader.Read(p)
+	return r.r.Read(p)
 }
 
 func TestProxyReader(t *testing.T) {
 	p := mpb.New(mpb.WithOutput(io.Discard))
-
-	tr := &testReader{strings.NewReader(content), false}
-
 	bar := p.New(int64(len(content)), mpb.NopStyle())
 
 	var buf bytes.Buffer
+	tr := &testReader{strings.NewReader(content), false}
 	_, err := io.Copy(&buf, bar.ProxyReader(tr))
 	if err != nil {
 		t.Errorf("io.Copy: %s\n", err.Error())
 	}
-
-	p.Wait()
 
 	if !tr.called {
 		t.Error("Read not called")
@@ -49,6 +46,7 @@ func TestProxyReader(t *testing.T) {
 	if got := buf.String(); got != content {
 		t.Errorf("Expected content: %s, got: %s\n", content, got)
 	}
+	p.Wait()
 }
 
 type testReadCloser struct {
@@ -63,49 +61,45 @@ func (r *testReadCloser) Close() error {
 
 func TestProxyReadCloser(t *testing.T) {
 	p := mpb.New(mpb.WithOutput(io.Discard))
-
-	tr := &testReadCloser{strings.NewReader(content), false}
-
 	bar := p.New(int64(len(content)), mpb.NopStyle())
 
+	tr := &testReadCloser{strings.NewReader(content), false}
 	rc := bar.ProxyReader(tr)
 	_, err := io.Copy(io.Discard, rc)
 	if err != nil {
 		t.Errorf("io.Copy: %s\n", err.Error())
 	}
 	_ = rc.Close()
-
-	p.Wait()
-
 	if !tr.called {
 		t.Error("Close not called")
 	}
+	p.Wait()
 }
 
 type testReaderWriterTo struct {
-	io.Reader
+	r      io.Reader
 	called bool
+}
+
+func (r *testReaderWriterTo) Read(p []byte) (n int, err error) {
+	return 0, errors.New("unexpected")
 }
 
 func (r *testReaderWriterTo) WriteTo(w io.Writer) (n int64, err error) {
 	r.called = true
-	return r.Reader.(io.WriterTo).WriteTo(w)
+	return r.r.(io.WriterTo).WriteTo(w)
 }
 
 func TestProxyReaderWriterTo(t *testing.T) {
 	p := mpb.New(mpb.WithOutput(io.Discard))
-
-	tr := &testReaderWriterTo{strings.NewReader(content), false}
-
 	bar := p.New(int64(len(content)), mpb.NopStyle())
 
 	var buf bytes.Buffer
+	tr := &testReaderWriterTo{strings.NewReader(content), false}
 	_, err := io.Copy(&buf, bar.ProxyReader(tr))
 	if err != nil {
 		t.Errorf("io.Copy: %s\n", err.Error())
 	}
-
-	p.Wait()
 
 	if !tr.called {
 		t.Error("WriteTo not called")
@@ -114,4 +108,5 @@ func TestProxyReaderWriterTo(t *testing.T) {
 	if got := buf.String(); got != content {
 		t.Errorf("Expected content: %s, got: %s\n", content, got)
 	}
+	p.Wait()
 }
