da75125b60265821aa229d0f49c110befe81c0ff
Vladimir Bauer <vbauerster@users.noreply.github.com>
2026-07-25 06:15:24 +0500

    minor: ConsoleWriter doc upd

--- console_writer.go
+++ console_writer.go
@@ -4,9 +4,8 @@ import "io"
 
 // ConsoleWriter interface.
 // mpb package interacts with terminal via ConsoleWriter interface.
-// Default implementation is "github.com/vbauerster/mpb/v8/cwriter"
-// package. Custom implementation can be provided via WithConsoleWriter
-// ContainerOption.
+// Default implementation is "github.com/vbauerster/mpb/v8/cwriter" package.
+// Custom implementation can be provided via WithConsoleWriter ContainerOption.
 type ConsoleWriter interface {
 	io.Writer
 	io.ReaderFrom
