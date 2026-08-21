86a1683ebcde9111e0ddcaadc63847f91915f84a
Vladimir Bauer <vbauerster@users.noreply.github.com>
2026-08-21 06:11:50 +0500

    refactoring: io example

--- _examples/io/main.go
+++ _examples/io/main.go
@@ -9,7 +9,8 @@ import (
 	"github.com/vbauerster/mpb/v8/decor"
 )
 
-const size = 32
+const size = 16
+const ewmaAge = 30
 
 func main() {
 	var total int64 = size * 1024 * 1024
@@ -18,11 +19,11 @@ func main() {
 
 	go func() {
 		for range 1024 {
-			_, err := io.Copy(w, io.LimitReader(rand.Reader, size*1024))
+			_, err := io.CopyN(w, rand.Reader, size*1024)
 			if err != nil {
 				panic(err)
 			}
-			time.Sleep(time.Second / 10)
+			time.Sleep(time.Second / 11)
 		}
 		_ = w.Close()
 	}()
@@ -35,20 +36,23 @@ func main() {
 			decor.Counters(decor.SizeB1024(0), "% .2f / % .2f"),
 		),
 		mpb.AppendDecorators(
-			decor.EwmaETA(decor.ET_STYLE_GO, 30),
+			decor.EwmaETA(decor.ET_STYLE_GO, ewmaAge),
 			decor.Name(" ] "),
-			decor.EwmaSpeed(decor.SizeB1024(0), "% .2f", 30),
+			decor.EwmaSpeed(decor.SizeB1024(0), "% .2f", ewmaAge),
 		),
 	)
 
 	// create proxy reader
-	proxyReader := bar.ProxyReader(r)
+	proxyReader, err := bar.ProxyReader(r)
+	if err != nil {
+		panic(err)
+	}
 	defer func() {
 		_ = proxyReader.Close()
 	}()
 
 	// copy from proxyReader, ignoring errors
-	_, err := io.Copy(io.Discard, proxyReader)
+	_, err = io.Copy(io.Discard, proxyReader)
 	if err != nil {
 		panic(err)
 	}
